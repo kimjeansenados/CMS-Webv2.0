34259a3bde2e2498e7b0ad7b32535c9f8ec45e6e
Mhine12 <arjay.tabangay@microbizone.com>
2017-04-18 09:23:30 +0800

    jhay

---
+++ CMS2.API/Controllers/ShipmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BAL = BusinessLogic;
+
+namespace CMS2.API.Controllers
+{
+    public class ShipmentController : ApiController
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["Shipment"].ConnectionString;
+        public List<BusinessLogic.Invoice> getinvoices([FromUri] DateTime start, [FromUri] DateTime end)
+        {
+            return BAL.Invoice.GetInvoices(start, end, connectionString);
+        }
+    }
+}
