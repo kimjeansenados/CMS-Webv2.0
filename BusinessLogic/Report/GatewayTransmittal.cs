0f47a7138113d0ef6ac6de5a166ec35caa2eebdb
msantos-microbiz <mark.santos@microbizone.com>
2017-05-23 13:22:20 +0800

    UPDATES 2

---
+++ BusinessLogic/Report/GatewayTransmittal.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using DAL = DataAccess;
+
+namespace BusinessLogic.Report
+{
+    public class GatewayTransmittal
+    {
+        public static DataSet GetGWTransmittal(string conSTR,string DateStr ,string GatewayStr ,string BCOStr ,string BatchStr)
+        {
+            return DAL.Reports.GatewayTransmittal.GetGatewayTransmittal(conSTR, DateStr, GatewayStr, BCOStr, BatchStr);
+        }
+
+        public static DataSet GetGWOutbound(string conSTR,string DateStr ,string GatewayStr ,string BCOStr ,string BatchStr)
+        {
+            return DAL.Reports.GatewayOutbound.GetGatewayOutbound(conSTR, DateStr, GatewayStr, BCOStr, BatchStr);
+        }
+
+        public static DataSet GetGWInbound(string conSTR, string DateStr,string GatewayStr,string BCOStr,string ComTypeStr)
+        {
+            return DAL.Reports.GatewayInbound.GetGatewayInbound(conSTR, DateStr, GatewayStr, BCOStr, ComTypeStr);
+        }
+        public static DataSet GetCargoTransfer(string conSTR , string date1, string origin, string destination)
+        {
+            return DAL.Reports.CargoTransfer.GetCargoTransfer(conSTR , date1, origin, destination );
+        }
+
+        public static DataSet GetHoldCargo(string conSTR)
+        {
+            return DAL.Reports.HoldCargo.GetGatewayOutbound(conSTR);
+        }
+        
+        public static DataSet GetGatewayList(string conSTR , string date)
+        {
+            return DAL.Reports.GatewayTransmittal.GetGatewayList(conSTR, date);
+        }
+
+        public static DataSet GetGatewayOutBoundList(string conSTR, string date)
+        {
+            return DAL.Reports.GatewayTransmittal.GetGatewayOutBoundList(conSTR, date);
+        }
+        public static DataSet GetGatewayInBoundList(string conSTR, string date)
+        {
+            return DAL.Reports.GatewayTransmittal.GetGatewayInBoundList(conSTR, date);
+        }
+
+    }
+}
